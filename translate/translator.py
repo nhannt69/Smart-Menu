00a43f570fb82d640861d929c3043693313b6ecb
MinhQuan <minhquan23102000@gmail.com>
2022-08-27 21:24:54 +0700

    add new translator

---
+++ translate/translator.py
@@ -0,0 +1,10 @@
+import json
+
+
+class DictionaryTranslator(object):
+    def __init__(self, dictionary_path):
+        with open(dictionary_path, 'r', encoding='utf-8') as f:
+            self.dictionary = json.load(f)
+
+    def translate(self, text):
+        return self.dictionary.get(text, text)
