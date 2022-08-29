b0967f5da62d23d378fbfa08daa028acd9e5dcdb
MinhQuan <minhquan23102000@gmail.com>
2022-08-29 14:40:12 +0700

    Reformat code evalution metric

--- test_evaluation/test_evaluation_tuple.py
+++ test_evaluation/test_evaluation_tuple.py
@@ -1,21 +1,20 @@
-import os
-import pandas as pd
 import json
+import os
+
+import pandas as pd
 
 
 class Metric():
 
-    def readFile(self):
-        result = pd.read_excel('data_sample\Data_Labeling.xlsx',  header=None)
-        return result
-  
-    def evaluation(self, result):
-    
-        dataframe = self.readFile()
-        dataframe_image = dataframe[dataframe[0] == result[0]]
+    def __init__(self, data_path='data_sample\Data_Labeling.xlsx'):
+        self.df = pd.read_excel(data_path, header=None)
 
-        print(dataframe_image)
-        
+    def evaluation(self, result, image_name):
+
+        dataframe_image = self.df[self.df[0] == image_name]
+
+        #print(dataframe_image)
+
         score = 0.0
 
         prediction, recall, f1_score = 0.0, 0.0, 0.0
@@ -28,29 +27,29 @@ class Metric():
 
         for obj in result[1::]:
             dataframe_namevi = None
-            print(obj[0])
+            #print(obj[0])
             dataframe_namevi = dataframe_image[dataframe_image[1] == obj[0].upper()]
 
             # print(dataframe_namevi)
-            
+
             dataframe_price = dataframe_namevi[dataframe_namevi[3] == obj[1]]
 
-            print(dataframe_price)
+            #print(dataframe_price)
 
             dataframe_nameen = dataframe_namevi[dataframe_namevi[2] == obj[2]]
-            
+
             if (dataframe_price.empty):
                 tn_count += 1
             else:
                 tp_count += 1
-            
+
             if (dataframe_nameen.empty):
                 tn_count_translate += 1
             else:
                 tp_count_translate += 1
-        
+
         #prediction
-        print(tp_count, tn_count)
+        #print(tp_count, tn_count)
         try:
             prediction = 1.0*tp_count/(tp_count + tn_count)
             prediction_translate = 1.0*tp_count_translate/(tp_count_translate + tn_count_translate)
@@ -66,11 +65,11 @@ class Metric():
         except:
             recall = 0.0
             recall_translate = 0.0
-        
+
         #f1 score
         try:
-            f1_score = 2.0*(prediction * recall)/(prediction + recall)  
-            print(prediction, recall)
+            f1_score = 2.0*(prediction * recall)/(prediction + recall)
+            #print(prediction, recall)
         except:
             pass
         try:
@@ -82,15 +81,14 @@ class Metric():
 
         return score, f1_score, f1_score_translate
 
-        
+
 
 if __name__ == "__main__":
     menu = ['002.jpeg',('Rau muống xào tỏi', '29000', 'SAUTEED WATER SPINACH WITH GARLIC'),('Cải xào tỏi', '29000', 'SAUTEED BORECOLE WITH GARLIC'),('Đậu xào', '145800', 'SAUTEED BORECOLE WITH GARLIC'),('Gà nướng', '45000', 'Chicken')]
-            
+
     evaluation = Metric()
-    
+
     print(evaluation.evaluation(result= menu))
-    
+
 
     # print(dataframe_image)
-                 
